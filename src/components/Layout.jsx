55256412031413a9fbd713ac352e0689ebb625a5
Akash Maurya <mauryaakash342@gmail.com>
2024-09-30 00:06:24 +0530

    Mobile View

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,144 +1,174 @@
-import { Link } from "react-router-dom"
+import { useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
 const Layout = ({children})=>{
+    const [open, setOpen] = useState(false)
+    const navigate = useNavigate()
+
     const menus =[
-    {
-        lable:"Home",
-        url:'/',
-        icone: <i className="ri-home-line"></i>
-    },
-    {
-        lable:"Products",
-        url:'/products',
-        icone:<i className="ri-product-hunt-fill"></i>,
-    },
-    {
-        lable:"Category",
-        url:'/category',
-        icone:<i className="ri-menu-search-line"></i>,
-    },
-    {
-        lable:"Contact-us",
-        url:'/contact-us',
-        icone:<i className="ri-contacts-book-line"></i>,
-    }  
-    ]
-    return(
+      {
+        label:"Home",
+          url:'/',
+          icone: <i className="ri-home-line"></i>
+      },
+      {
+        label:"Products",
+          url:'/products',
+          icone:<i className="ri-product-hunt-fill"></i>,
+      },
+      {
+        label:"Category",
+          url:'/category',
+          icone:<i className="ri-menu-search-line"></i>,
+      },
+      {
+        label:"Contact-us",
+          url:'/contact-us',
+          icone:<i className="ri-contacts-book-line"></i>,
+      }  
+      ]
 
-  <div>
-<nav className="shadow-lg bg-[#f6f6f6] sticky top-0 left-0">
-    <div className="w-10/12 mx-auto flex items-center justify-between">
-    <img src="./logo.png"
-     className="w-[90px] py-2"   
-    />
-    <ul className="flex gap-3 items-center">
-        {
-    menus.map((item,index)=>(
- 
-    <li key={index}>
-        <Link 
-        className="block py-4 text-center hover:text-white hover:bg-blue-600 w-[100px]"
-        to={item.url}>
-        {item.lable}
-        </Link>
-    </li>
+    const mobileLink = (href)=>{
+        navigate(href)
+        setOpen(false)
+    }
 
-    ))
-        }
+    return (
+        <div>
+            <nav className="sticky top-0 left-0 shadow-lg bg-white">
+                <div className="w-10/12 mx-auto flex items-center justify-between">
+                    <img 
+                        src="/logo.png"
+                        className="w-[100px]"
+                    />
+                    
+                    <button className="md:hidden" onClick={()=>setOpen(!open)}>
+                        <i className="ri-menu-3-fill text-3xl"></i>
+                    </button>
 
-      <li> <Link to="/login"  className=" py-4 text-center hover:text-white hover:bg-blue-600 w-[100px]"> Login</Link></li>  
-      <li>  <Link to="/register"  className="bg-blue-600 py-3 px-10 text-md font-semiblod rounded  text-white block text-center hover:text-white hover:bg-rose-600 "> Register</Link></li>  
+                    <ul className="md:flex gap-6 items-center hidden">
+                        {
+                            menus.map((item, index)=>(
+                                <li key={index}>
+                                    <Link 
+                                        to={item.url}
+                                         className="block py-4 text-center hover:text-white hover:bg-blue-600 w-[100px]"
+                                    >{item.label}</Link>
+                                </li>
+                            ))
+                        }
+                        <Link 
+                            className="block py-4 text-center hover:text-white hover:bg-blue-600 w-[100px]"
+                            to="/login"
+                        >Login</Link>
 
-    </ul>
-    </div>
+                        <Link 
+                            className="bg-blue-600 py-3 px-10 text-md font-semibold text-white block text-center hover:bg-rose-600 hover:text-white"
+                            to="/signup"
+                        >Register</Link>
+                    </ul>
+                </div>
+            </nav>
+            <div>
+                {children}
+            </div>
+            <footer className="bg-black text-white py-16 ">
+                <div className="w-10/12 mx-auto grid md:grid-cols-4 md:gap-0 gap-8">
+                    <div>
+                        <h1 className="text-white font-semibold text-2xl mb-3">Website Links</h1>
+                        <ul className="space-y-2 text-slate-50">
+                            {
+                                menus.map((item, index)=>(
+                                    <li key={index}>
+                                        <Link to={item.href}>{item.icone} {item.label}</Link>
+                                    </li>
+                                ))
+                            }
+                            <li><Link to="/login"> <i className="ri-login-box-line"></i> Login</Link></li>
+                            <li><Link to="/signup"> <i className="ri-registered-line"></i> Register</Link></li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h1 className="text-white font-semibold text-2xl mb-3">Follow us</h1>
+                        <ul className="space-y-2 text-slate-50">
+                        <li> <Link to="/"> <i className="ri-facebook-circle-line"></i> Facebook</Link></li>  
+                        <li> <Link to="/"> <i className="ri-youtube-line"></i> Youtube</Link></li>  
+                        <li> <Link to="/"><i className="ri-twitter-x-line"></i>  Twitter</Link></li>  
+                        <li> <Link to="/"> <i className="ri-linkedin-box-line"></i> Linkedin</Link></li>  
+                        <li> <Link to="/"> <i className="ri-instagram-line"></i> Instagram</Link></li>  
+                        </ul>
+                    </div>
+                    <div className="pr-8">
+                        <h1 className="text-white font-semibold text-2xl mb-3">Brand Details</h1>
+                        <p className="text-slate-50 mb-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad saepe cupidium est velit excepturi sit corrupti tempora officia recusandae!</p>
+                        <img 
+                            src="/logo.png"
+                            className="w-[100px]"
+                        />
+                    </div>
+                    <div>
+                        <h1 className="text-white font-semibold text-2xl mb-3">Contact us</h1>
+                        <form className="space-y-4">
+                            <input 
+                                required
+                                name="fullname"
+                                className="bg-white w-full rounded p-3"
+                                placeholder="Your name"
+                            />
 
-</nav>
-  
- <div>
- {children}
- </div>
+                            <input 
+                                required
+                                type="email"
+                                name="email"
+                                className="bg-white w-full rounded p-3"
+                                placeholder="Enter email id"
+                            />
 
-  <footer className="bg-black text-white py-16 ">
-  <div className="w-10/12 mx-auto items-center justify-between grid grid-cols-4 gap-2">
-  <div> 
-    <h1 className=" font-semibold text-2xl mb-4">Brand Details</h1>
-  <p className="text-gray-100 mb-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint natus dolorem soluta voluptatum temporibus quis maiores provident id explicabo, vitae eos, facilis nulla a unde odit ex illum perferendis facere!</p>
-    <img src="./logo.png"
-     className="w-[90px] py-2"   
-    />
-   
-    </div>
-    <div className="mt-[-18%] ml-10"> 
-    <h1 className=" font-semibold text-2xl">Website Link</h1>
-    <ul className="space-y-2 ">
-        {
-        menus.map((item,index)=>(
-       <li key={index}  >
-        <Link to={item.url}> {item.icone} {item.lable}</Link>
-       </li>  
-            
-        ))
-        }
-             <li> <Link to="/login"> <i className="ri-login-box-line"></i> Login</Link></li>  
-             <li>  <Link to="/register"><i className="ri-registered-line"></i>  Register</Link></li>  
-    </ul>
- 
-   
-    </div>
+                            <textarea 
+                                required
+                                name="message"
+                                className="bg-white w-full rounded p-3"
+                                placeholder="Message"
+                                rows={3}
+                            />
 
+                  <button type="Submit"  className="bg-blue-500 text-white py-3 px-6 rounded">Submit</button>
+                        </form>
+                    </div>
+                </div>
+            </footer>
 
-    <div className="mt-[-30%] ml-10"> 
-    <h1 className=" font-semibold text-2xl  ">Follow Us</h1>
-    <ul className="space-y-2 ">
-       
-             <li> <Link to="/"> <i className="ri-facebook-circle-line"></i> Facebook</Link></li>  
-             <li> <Link to="/"> <i className="ri-youtube-line"></i> Youtube</Link></li>  
-             <li> <Link to="/"><i className="ri-twitter-x-line"></i>  Twitter</Link></li>  
-             <li> <Link to="/"> <i className="ri-linkedin-box-line"></i> Linkedin</Link></li>  
-             <li> <Link to="/"> <i className="ri-instagram-line"></i> Instagram</Link></li>  
-    </ul>
- 
-   
-    </div>
+           
+            <aside 
+                className="overflow-hidden md:hidden bg-slate-900 shadow-lg fixed top-0 left-0 h-full z-50"
+                style={{
+                    width: open ? 250 : 0,
+                    transition: '0.3s'
+                }}
+            >
+                <div className="flex flex-col gap-6">
+                    {
+                        menus.map((item, index)=>(
+                            <button onClick={()=>mobileLink(item.url)} key={index} className="text-white">
+                              {item.icone}  {item.label}
+                            </button>
+                        ))
+                    }
+                      
+                            <Link 
+                            className="bg-blue-600 ml-12 w-40 text-center py-3 px-10 text-md font-semibold text-white block text-center hover:bg-rose-600 hover:text-white"
+                            to="/login"
+                        >Login</Link>
 
 
 
-    <div className="gap-5"> 
-   <h1 className="font-semibold text-2xl mb-3">Contact US </h1>
-   <form className="space-y-2">
-
-    <input 
-     name="fullname"
-     className="bg-ehite w-full rounded p-3"
-     placeholder="Your Name"
-     required
-     />
-
-<input 
-     name="email"
-     className="bg-ehite w-full rounded p-3"
-     placeholder="Your Email Id"
-     required
-     />
-
-     <textarea 
-     required
-      name="message"  
-      className="bg-ehite w-full rounded p-3"
-        placeholder="Your Message .... "
-      rows={3}
-      />
-      <button type="Submit"  className="bg-blue-500 text-white py-3 px-6 rounded">Submit</button>
-   </form>
-    </div>
-
-
-  </div>
-
-
-  </footer>
-  
-  </div>
-
+                        <Link 
+                            className="bg-blue-600 ml-12 w-40 py-3 px-10 text-md font-semibold text-white block text-center hover:bg-rose-600 hover:text-white"
+                            to="/signup"
+                        >Register</Link>
+                </div>
+            </aside>
+        </div>
     )
 }
+
 export default Layout
